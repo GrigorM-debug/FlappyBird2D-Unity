97e30e74456a1a2b3a72a4684d255e3d99e2702f
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-05-20 21:53:03 +0300

    Implemented Looping Background

---
+++ Flappy Bird/Assets/Scrips/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] public float scrollSpeed = 2.0f;
+    private float offset;
+    private Material material;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;    
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        offset += (Time.deltaTime * scrollSpeed) / 10f;
+        material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}

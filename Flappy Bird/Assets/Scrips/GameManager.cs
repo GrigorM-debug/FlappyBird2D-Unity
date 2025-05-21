a71a6b0ba912efc0dc8105a3faac4a5bbc3a9c32
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2025-05-22 02:36:09 +0300

    Fix the score displaying problem

--- Flappy Bird/Assets/Scrips/GameManager.cs
+++ Flappy Bird/Assets/Scrips/GameManager.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int score;
 
-    private Text scoreText;
+    [SerializeField] private Text scoreText;
 
     public void IncreaseScore()
     {
         score++;
+
+        Debug.Log($"Score: {score}");
+
         scoreText.text = score.ToString();
     }
 
     public void GameOver()
     {
         ScoreManager.TryUpdateBestScore(score);
-        //Show the gameover scene
+
+        //Load the gameover scene
+        SceneManager.LoadScene("FlappyBird.GameOver");
     }
 }
